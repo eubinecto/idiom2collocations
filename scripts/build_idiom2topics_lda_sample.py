99bb2d55dd71788a05aeb985b10173f3fd69423f
Eu-Bin KIM <tlrndk123@gmail.com>
2021-02-05 17:08:05 +0900

    topic modeling with lda done. still, need a way of saving the model (with metadata), the topics, and idiom2topics all at the same time, with properly annonated date.

--- scripts/build_idiom2topics_lda_sample.py
+++ scripts/build_idiom2topics_lda_sample.py
@@ -1,9 +1,9 @@
 import csv
-from pprint import PrettyPrinter
+import json
 from gensim.corpora import Dictionary
 from gensim.models import LdaMulticore
 from loaders import Idiom2ContextLoader
-from idiom2topics.config import RESULTS_SAMPLE_IDIOM2CONTEXT_TSV
+from idiom2topics.config import RESULTS_SAMPLE_IDIOM2CONTEXT_TSV, RESULTS_SAMPLE_IDIOM2TOPICS_LDA_TSV
 import logging
 from sys import stdout
 from functional import seq
@@ -14,6 +14,7 @@ logging.basicConfig(stream=stdout, level=logging.DEBUG)
 def main():
     # a generator. - list of tuples.
     idiom2context = Idiom2ContextLoader(RESULTS_SAMPLE_IDIOM2CONTEXT_TSV).load()
+    # turning them into a list, as we need to iterate over them more than once.
     idiom2docs = seq(idiom2context).group_by_key().to_list()  # idiom, context -> group by idiom.
     # load the docs.
     # question - how do I stream a document.. for building a topic modeling?
@@ -21,7 +22,7 @@ def main():
         doc
         for _, docs in idiom2docs
         for doc in docs
-    ]
+    ]  # flatten out all the docs
     # need all of them to be loaded to build a dictionary.
     dct = Dictionary(documents=docs)
     # build bag-of-words representations.
@@ -29,7 +30,6 @@ def main():
         dct.doc2bow(doc, allow_update=True)
         for doc in docs
     ]
-
     # TODO: diff between iterations & passes? I see "passes" = "epochs", but..
     # so.. it was a matter of tuning this hyper parameter.
     # I certainly don't want to have a model for each idiom.
@@ -39,10 +39,24 @@ def main():
                              # for debugging and topic printing, we need to give it this.
                              id2word=dct,
                              num_topics=100,
-                             passes=10)
+                             passes=4)
     # have a guess, what are those topics? - how do I see them...?
-    for topic_id, idx2prob in lda_model.show_topics(num_topics=100, num_words=10, formatted=True):
-        print(topic_id, "--->", idx2prob)
+    with open(RESULTS_SAMPLE_IDIOM2TOPICS_LDA_TSV, 'w') as fh:
+        tsv_writer = csv.writer(fh, delimiter="\t")
+        for idiom, docs in idiom2docs:
+            bows = [dct.doc2bow(doc) for doc in docs]
+            topic_vectors = [
+                [prob for _, prob in lda_model.get_document_topics(bow, minimum_probability=0)]
+                for bow in bows
+            ]
+            # should be able to do this...
+            topic_mat = np.array(topic_vectors)
+            # TODO: use gaussian distribution over each column, instead of just using the mean.
+            # then, you could use the probability of getting a scalar from that distribution (the output of pdf)
+            # as how close each example is.
+            idiom_topic_vector = topic_mat.mean(axis=0)  # mean over each column
+            to_write = [idiom, json.dumps(idiom_topic_vector.tolist())]
+            tsv_writer.writerow(to_write)   # write them.. this is essentially the representation of them.
 
 
 if __name__ == '__main__':
